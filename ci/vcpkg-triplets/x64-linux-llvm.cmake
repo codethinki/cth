8e1417f3cac5d785d31a7b8246919b2ebe572305
codethinki <lukas.jmy@outlook.de>
2026-01-30 19:51:19 +0100

    trying to use libc++ for linux

---
+++ ci/vcpkg-triplets/x64-linux-llvm.cmake
@@ -0,0 +1,10 @@
+set(VCPKG_TARGET_ARCHITECTURE x64)
+set(VCPKG_CRT_LINKAGE dynamic)
+set(VCPKG_LIBRARY_LINKAGE dynamic)
+set(VCPKG_CMAKE_SYSTEM_NAME Linux)
+
+set(VCPKG_ENV_PASSTHROUGH CC CXX)
+
+set(VCPKG_CXX_FLAGS "-stdlib=libc++")
+set(VCPKG_C_FLAGS "-stdlib=libc++")
+set(VCPKG_LINKER_FLAGS "-stdlib=libc++ -lc++abi")
